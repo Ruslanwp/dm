395195ba5ec3b8c41eb6325e80bb6f4391daae77
Ruslan Shmadchenko <ruslan.shmadchenko@cobrainer.com>
2024-09-12 21:29:07 +0200

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type AppProps = {
+  name: string;
+}
+
+function App() {
+  return (
+    <div className="App">
+      123
+    </div>
+  );
+}
+
+export default App;
